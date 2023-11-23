41f0a3362a2ee762168d1ecbc24f6229756e716a
Matti Tahvonen <matti@vaadin.com>
2023-11-23 14:08:58 +0200

    Added e2e smoke test that runs through all test views...
    ... and ensures they
    
    * open properly
    * throw no JS exceptions

---
+++ src/test/java/org/vaadin/tinymce/MopoSmokeIT.java
@@ -0,0 +1,56 @@
+package org.vaadin.tinymce;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import in.virit.mopo.Mopo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MopoSmokeIT {
+
+    private final int port = 9998;
+
+    private final String rootUrl = "http://localhost:" + port + "/";
+
+    static Playwright playwright = Playwright.create();
+
+    static {
+    }
+
+    private Browser browser;
+    private Page page;
+    private Mopo mopo;
+
+    @BeforeEach
+    public void setup() {
+        browser = playwright.chromium()
+                .launch(new BrowserType.LaunchOptions()
+//                        .setHeadless(false)
+//                        .setDevtools(true)
+                );
+
+        page = browser.newPage();
+        page.setDefaultTimeout(5000); // die faster if needed
+        mopo = new Mopo(page);
+    }
+
+    @AfterEach
+    public  void closePlaywright() {
+        page.close();
+        browser.close();
+    }
+
+    @Test
+    public void smokeTest() {
+        mopo.getViewsReportedByDevMode(browser, rootUrl).forEach(viewName -> {
+            String url = rootUrl + viewName;
+            page.navigate(url);
+            mopo.assertNoJsErrors();
+            System.out.println("Checked %s and it contained no JS errors.".formatted(viewName));
+        });
+
+    }
+}
