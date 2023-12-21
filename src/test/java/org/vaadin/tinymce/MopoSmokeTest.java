088915d3affb5be54e634dbad75b30ca867d6805
Matti Tahvonen <matti@vaadin.com>
2023-12-21 13:43:27 +0200

    Cleanup + version updates

--- src/test/java/org/vaadin/tinymce/MopoSmokeTest.java
+++ src/test/java/org/vaadin/tinymce/MopoSmokeTest.java
@@ -10,10 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class MopoSmokeIT {
+public class MopoSmokeTest {
 
     @LocalServerPort
     private int port;
